7fba0b1a05f4343c5984c85bdfdebbd2ad1ad0ab
Albert Drobakha <albert.drobakha@gmail.com>
2026-04-09 11:22:01 +0300

    html cow + js db

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("what???")
+
+async function loadWorld() {
+	const response = await fetch('https://tinkr.tech/sdb/poly/wander');
+	const documents = await response.json();
+	console.log(documents); // Array of all documents
+}
