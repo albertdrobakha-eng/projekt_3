fc06579ceffd78b73611a690154f75c978f3b074
albertdrobakha-eng <albertdrobakha@gmail.com>
2026-04-16 11:16:04 +0300

    js cow+px

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-console.log("what???")
+console.log("Wworld")
 
-const newDiv = document.createElement('div');
-const newButton = document.createElement('button');
-const newParagraph = document.createElement('p');
+
 
 
 
 async function loadWorld() {
-	const response = await fetch('https://tinkr.tech/sdb/poly/wander');
-	const documents = await response.json();
-	console.log(documents); // Array of all documents
+  const response = await fetch('https://tinkr.tech/sdb/poly/wander');
+  const state = await response.json();
+
+  console.log(state);
+  return state;
 }
+
+loadWorld().then(state => {
+  const world = document.getElementById('world');
+
+  for (const player of state.players) {
+    const playerDiv = document.createElement('div');
+
+    playerDiv.style.position = 'absolute';
+    playerDiv.style.left = player.x + 'px';
+    playerDiv.style.top = player.y + 'px';
+
+    const img = document.createElement('img');
+
+  
+    img.src = 'https://tinkr.tech' + player.image;
+
+    img.width = player.width;
+    img.height = player.height;
+
+    playerDiv.appendChild(img);
+    world.appendChild(playerDiv);
+  }
+});
+
+
+
+
+
+
+
+
+
+const newDiv = document.createElement('div');
+newDiv.textContent = 'I am new!';
+
+parent.appendChild(newDiv);
+
+
+
 
 
 // Set properties
@@ -21,18 +60,13 @@ newDiv.classList.add('my-class');
 document.body.appendChild(newDiv);
 
 
-const parent = document.querySelector('.container');
-const newDiv = document.createElement('div');
-newDiv.textContent = 'I am new!';
-
-parent.appendChild(newDiv);
-
 const timer = setInterval(function() {
   console.log("Running...");
 }, 1000);
 
 // Later, stop it:
 clearInterval(timer);
+
 
 
 
